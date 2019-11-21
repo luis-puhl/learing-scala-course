26e457a2abeb154d86024e68eda1442e76e50d34
Luis Puhl <luispuhl@gmail.com>
2019-11-21 09:58:10 -0300

    Lecture 1

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,27 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+  val x: Int = 42
+  println(x)
+
+  // x = 2 // !error
+  // vals are immutable
+
+  val y = 42
+  // types can be inferred
+
+  val text: String = "Hello";
+  // instruction separator ";" OR "newline". former is preferred.
+  val isGood: Boolean = true
+  val key: Char = 'c'
+  val short2Bytes: Short = 2
+  val int4Bytes: Int = 1324123
+  val long8Bytes: Long =  121231231324L
+  val float: Float = 2.0f
+  val duble: Double = 3.14
+
+  // variables
+  var aVar: Int = 10
+  aVar = 20
+  // side effects
+}
