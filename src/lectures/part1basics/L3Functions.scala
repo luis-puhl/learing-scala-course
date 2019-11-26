063c2c43df4740d34b3be2b39c9bf087c7bb2b54
Luis Puhl <luispuhl@gmail.com>
2019-11-26 16:40:45 -0300

    Lecuture 3 Functions

---
+++ src/lectures/part1basics/L3Functions.scala
@@ -0,0 +1,30 @@
+package lectures.part1basics
+
+object L3Functions extends App {
+  def aFunction(a: String, b: Int): String =
+    a + " " + b
+  // def is a single expression
+  def aFunction2(a: String, b: Int): String = {
+    a + " " + b
+  }
+
+  println(aFunction("Hello", 1))
+
+  def aParamaterlessFunction(): Int = 42
+  println(aParamaterlessFunction())
+  println(aParamaterlessFunction)
+
+  /** When you need loops, use recursions */
+  def aRepeteadFunction(a: String, n: Int): String = {
+    if (n==1) a
+    else a + aRepeteadFunction(a, n -1)
+  }
+  println(aRepeteadFunction("-", 80))
+
+  def aFuncWithSideEffects(a: String): Unit = println(a)
+
+  def bigFunc(n: Int): Int = {
+    def aSmallFunc(a: Int, b: Int): Int = a + b
+    aSmallFunc(n, n-1)
+  }
+}
