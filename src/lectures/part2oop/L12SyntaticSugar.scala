da08942bfe9b418024ea0e326997749455b50e59
Luis Puhl <luispuhl@gmail.com>
2019-11-28 19:12:15 -0300

    Lesson 13: Method Notatins Exercices

--- src/lectures/part2oop/L12SyntaticSugar.scala
+++ src/lectures/part2oop/L12SyntaticSugar.scala
@@ -1,13 +1,18 @@
 package lectures.part2oop
 
 object L12SyntaticSugar extends App {
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, favoriteMovie: String, val age: Int = 20) {
     def likes(movie: String): Boolean = movie == favoriteMovie
     def hangsOutWith(p: Person): String = s"${this.name} is hanging out with ${p.name}"
     def +(p: Person): String = this.hangsOutWith(p)
+    def +(s: String): Person = new Person(s"$name ($s)", favoriteMovie)
     def unary_! : String =  s"$name: what the heck?!"
     def isAlive: Boolean = true
     def apply(): String = s"Hi, my name is $name and I like $favoriteMovie."
+    def apply(n: Int): String = s"$name watched $favoriteMovie $n times"
+    def unary_+ : Person = new Person(name, favoriteMovie, age+1)
+    def learns(what: String): String = f"$name learns $what"
+    def learnsScala: String = learns("Scala")
   }
 
   val mary = new Person("Mary", "Inception")
@@ -35,4 +40,31 @@ object L12SyntaticSugar extends App {
   // apply
   println(mary.apply)
   println(mary())
+
+  /*
+  1. Overload the + operator
+      mary + "the rockstar" => new person "Mary (the rockstar)"
+   */
+  println((mary + "movie lover").apply())
+
+  /*
+    2. Add an age to person class
+     - unary operator + => new person with age+1
+     */
+  val maryb = +mary
+  println(maryb.age)
+
+  /*
+  3. Add learns method, returns "Mary learns Scala"
+   - add overload with Scala use with postfix
+   */
+  println(mary.learns("java"))
+  println(mary learns "js")
+  println(mary learnsScala)
+
+  /*
+  4. Overload the apply method
+    - mary(2) => "Mary watched inception 2 times"
+   */
+  println(maryb(10))
 }
