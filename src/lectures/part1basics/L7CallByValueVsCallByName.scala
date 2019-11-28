890e077cfd4fb19928434da7dbe4607989333856
Luis Puhl <luispuhl@gmail.com>
2019-11-28 17:08:11 -0300

    lession 7 Call by Name [ () => Int ] -> [ x: => Int]

---
+++ src/lectures/part1basics/L7CallByValueVsCallByName.scala
@@ -0,0 +1,26 @@
+package lectures.part1basics
+
+object L7CallByValueVsCallByName  extends App {
+  def byValue(x: Long): Unit = {
+    println(s"by value: $x")
+    println(s"by value: $x")
+  }
+  def byName(x: => Long): Unit = {
+    println(s"by name: $x")
+    println(s"by name: $x")
+  }
+  // prints 2x same time
+  byValue(System.nanoTime())
+  // prints 2 different values
+  byName(System.nanoTime())
+
+  def infinite(): Int = 1 + infinite()
+  def print1s(x: Int, y: => Int): Unit = println(x)
+
+  // breaks, infinite call
+  print1s(infinite(), 12)
+
+  // does not break because the y is eval is delayed
+  // and as y is not used, infinite is never called
+  print1s(12, infinite())
+}
