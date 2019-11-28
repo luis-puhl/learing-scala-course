f6889ae05e0ece910910dae134339bd1eba4a163
Luis Puhl <luispuhl@gmail.com>
2019-11-28 18:00:54 -0300

    Lesson 11 exercices: my solution

--- src/lectures/part2oop/L10ObjectOrientedBasics.scala
+++ src/lectures/part2oop/L10ObjectOrientedBasics.scala
@@ -26,3 +26,36 @@ class Person(name: String, val age: Int) {
   def this(name: String) = this(name, 0)
   def this() = this("John Doe")
 }
+
+/*
+Novel and Writer
+
+Writer: first name, surname, year
+ - fullname
+
+Novel: name, year of release, author
+ - authorAge
+ - isWrittenBy
+ - copy (new year of release) = new instance of Novel
+ */
+class Writer(val name: String, val surname: String, val year: Int) {
+  def fullName(): String = s"${surname.toUpperCase()}, ${name.take(1)}"
+}
+class Novel(val name: String, val author: Writer, val yearOfRelease: Int) {
+  def authorAnge(): Int = author.year - yearOfRelease
+  def isWrittenBy(author: Writer): Boolean = this.author == author
+  def copy(yearOfRelease: Int): Novel = new Novel(name, author, yearOfRelease)
+}
+
+/*
+Counter class
+ - receives an int value
+ - method current count
+ - method to increment/decrement => new Counter
+ - overload inc/dec to receive an amount
+ */
+class Counter(val value: Int = 0) {
+  def current(): Int = value
+  def inc(i: Int = 1) = new Counter(value + i)
+  def dec(i: Int = 1) = new Counter(value - i)
+}
