dc62fe7e6862c190d82e5c31b52497563ab21d22
Luis Puhl <luispuhl@gmail.com>
2019-11-29 23:08:21 -0300

    Lesson 19 Anonymous classes

---
+++ src/lectures/part2oop/L19AnonymousClasses.scala
@@ -0,0 +1,21 @@
+package lectures.part2oop
+
+object L19AnonymousClasses extends App {
+  abstract class Animal {
+    def eat: Unit
+  }
+  // anonymous class
+  val funnyAnimal: Animal = new Animal {
+    override def eat: Unit = println("hahhaahhahhaha")
+  }
+  println(funnyAnimal.getClass)
+
+  class Person(name: String) {
+    def sayHy: Unit = println(s"Hi, my name is $name, how can I help you?")
+  }
+  val jim = new Person("Jim") {
+    override def sayHy: Unit = println(s"Hi, my name is Jim, how can I be of service?")
+  }
+  println(jim.getClass)
+  jim.sayHy
+}
