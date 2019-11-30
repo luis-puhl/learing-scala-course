c2a85a449c1021d9a3bddd01e54dcf9b4a4b9c45
Luis Puhl <luispuhl@gmail.com>
2019-11-29 23:02:42 -0300

    Lesson 18: Generics and Variance Type Problems

--- src/exercices/MyList.scala
+++ src/exercices/MyList.scala
@@ -10,34 +10,34 @@ import scala.runtime.Nothing$
  * - add(int)
  * - toString
  */
-abstract class MyList {
-  def head: Int
-  def tail: MyList
+abstract class MyList[+A] {
+  def head: A
+  def tail: MyList[A]
   def isEmpty: Boolean
-  def add(x: Int): MyList
+  def add[B >: A](x: B): MyList[B]
   def printElements: String
   override def toString: String = s"[$printElements]"
 }
 
-object Empty extends MyList {
-  def head: Int = throw new NoSuchElementException
-  def tail: MyList = throw new NoSuchElementException
+object Empty extends MyList[Nothing] {
+  def head: Nothing = throw new NoSuchElementException
+  def tail: MyList[Nothing] = throw new NoSuchElementException
   def isEmpty: Boolean = true
-  def add(x: Int): MyList = new Const(x, Empty)
+  def add[A >: Nothing](x: A): MyList[A] = new Const[A](x, Empty)
   override def printElements: String = ""
 }
-class Const(val headVal: Int, val tailVal: MyList = Empty) extends MyList {
-  override def head: Int = headVal
-  override def tail: MyList = tailVal
+class Const[+A](headVal: A, val tailVal: MyList[A] = Empty) extends MyList[A] {
+  override def head: A = headVal
+  override def tail: MyList[A] = tailVal
   override def isEmpty: Boolean = false
-  override def add(x: Int): MyList = new Const(x, this)
+  override def add[B >: A](x: B): MyList[B] = new Const[B](x, this)
   override def printElements: String =
     if (tailVal.isEmpty) headVal.toString
     else tailVal.printElements + ", " + headVal.toString
 }
 
 object ListTest extends App {
-  def test(l: MyList) = {
+  def test[A](l: MyList[A]) = {
     try println(l.head) catch {
       case _: Throwable => println("no head")
     }
@@ -45,15 +45,24 @@ object ListTest extends App {
       case _: Throwable => println("no tail")
     }
     println(l.isEmpty)
-    println(l.add(10).toString)
+    println(l.toString)
   }
   println("Testing empty")
   test(Empty)
   println("Testing 1")
   val l1 = new Const(1)
   test(l1)
+
   println("Testing 2")
   val l2 = new Const(2, l1)
   test(l2)
   test(l2 add 3 add 4)
+
+  println("Testing 3")
+  val l3 = new Const("")
+  test(l3)
+  val l4 = l3 add "a" add "b"
+  test(l4)
+  val l5 = l4.add(10).add(20)
+  test(l5)
 }
---
+++ src/lectures/part2oop/L18Generics.scala
@@ -0,0 +1,61 @@
+package lectures.part2oop
+
+object L18Generics extends App {
+  class MyList[A] {
+    // use the type A
+    def add(a: A): MyList[A] = ???
+  }
+  class MyMap[Key, Value]
+  val a = new MyList[Int]
+  val b = new MyList[String]
+
+  // generic methods
+  object MyList {
+    def empty[A]: MyList[A] = ???
+  }
+  val c = MyList.empty[Int]
+
+  // variance problem
+  class Animal
+  class Cat extends Animal
+  class Dog extends Animal
+
+  // List[Cat] extends List[Animal] ???
+
+  // Covariance
+  class CovariantList[+A]
+  val animal: Animal = new Cat
+  val animalList: CovariantList[Animal] = new CovariantList[Cat]
+//  animalList.add(new Dog)
+  // hard question
+  // pollutes the typed list
+
+  // Invariance
+  class InvariantList[A]
+  val invar: InvariantList[Animal] = new InvariantList[Animal]
+
+  // ContraVariance
+  class ContravariantList[-A]
+  val contra: ContravariantList[Cat] = new ContravariantList[Animal]
+  // another example
+  class Trainer[-A]
+  val trainer: Trainer[Cat] = new Trainer[Animal]
+
+  // accepts only subclass of animal
+  // A is a child of Animal
+  class Cage[A <: Animal] (animal: A)
+  val cage = new Cage(new Dog)
+
+  class Car
+  // generic type needs proper bounded type
+  // val cageFail = new Cage(new Car)
+
+  class MyListCovariant[+A] {
+    // B is parent of A
+    def add[B >: A](a: B): MyList[B] = ???
+    /*
+    A = Cat
+    B = Animal
+     */
+  }
+}
